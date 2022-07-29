ff29ea252883fe7ee8685112389fe9578c72de64
iEdgir01 <fgteiw@gmail.com>
2022-07-29 13:27:11 +0200

    completed the function

--- increase-decrease/Increase - Decrease.py
+++ increase-decrease/Increase - Decrease.py
@@ -3,3 +3,21 @@
 #the directions he got are a little confusing. He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
 #An opening parenthesis, (, means he should go up one floor, and a closing parenthesis, ), means he should go down one floor.
 #https://adventofcode.com/2015/day/1
+
+def increase_decrease(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    int_val = 0
+    for i in range(len(s)):
+        if i > 0 and s[i] == ')':
+            int_val -= 1
+        elif i > 0 and s[i] == '(':
+            int_val += 1
+        else:
+            int_val += 1
+    return int_val
+
+input_str = input("Enter a string: ")
+print(f"The floor santa ends on is: {increase_decrease(input_str)}")
--- increase-decrease/readme.md
+++ increase-decrease/readme.md
@@ -1,4 +1,2 @@
 the purpose of the increase Decrease code is to start at 0 and count up by 1 for every determined char in a string, 
 and to count down by 1 for any other determined char in a string.
-
-#in progress.
